5b1f9f708c367d7ec50022f23e4dd3e76721fa97
barzerman <barzerman-noreply@github.com>
2020-04-28 01:09:33 -0400

    traversers, bugfixes

--- tree/dma_heap.h
+++ tree/dma_heap.h
@@ -7,6 +7,8 @@
 
 #include <functional>
 #include <vector>
+#include <stack>
+#include <queue>
 
 // array heap implementation with direct access to elements
 // and position tracing. every new element placed in the array
@@ -17,27 +19,47 @@
 //              must have void operator(int) {} defined
 
 using identity = struct {void operator()(int){};};
-template <typename DataType, typename Compare=std::less<DataType>, typename PosTracker = identity>
+template <typename DataType, typename Compare=std::less<>, typename PosTracker = identity>
 class dma_heap {
 public:
-    using value_type = std::pair<DataType, PosTracker>;
+    using tracker_type = PosTracker;
+    using data_type = DataType;
+    using value_type = std::pair<data_type, tracker_type>;
     using vector_type = typename std::vector<value_type>;
     using size_type = typename vector_type::size_type;
+    using children_type = std::pair<size_type, size_type>;
 
-    void push_heap(value_type&& v) {
-        if (vec_.empty()) {
-            vec_.emplace_back(value_type());
+    static const size_type ROOT_POS = 1;
+    using const_iterator = typename vector_type::const_iterator;
+
+    const_iterator begin() const { return vec_.begin()+1;}
+    const_iterator end() const { return vec_.end();}
+
+    dma_heap() = default;
+    dma_heap(const dma_heap&) = default;
+    dma_heap(dma_heap&&)  noexcept = default;
+    dma_heap& operator =(dma_heap&&)  noexcept = default;
+
+    template <typename Iter>
+    dma_heap(Iter begin, Iter end) {
+        for(auto i = begin; i != end; i++ ) {
+            push_heap(*i);
         }
-        vec_.emplace_back(v);
-        filter_down_();
     }
-    void push_heap(DataType&& dt, PosTracker pt=identity()) {
-        push_heap(value_type {dt, pt});
+    template<typename U = data_type>
+    void push_heap(U&& dt, PosTracker&& pt=identity()) {
+        static_assert(std::is_same_v<std::decay_t<U>, std::decay_t<DataType>>);
+
+        if (vec_.empty())
+            vec_.resize(1);
+        vec_.emplace_back(std::move(value_type {dt, pt}));
+        filter_up_();
     }
+
     // pops the top element
     value_type&& pop_heap() {
         vec_[1] = vec_.pop_back();
-        filter_up_();
+        filter_down_();
     }
     value_type&& peek_by_pos(size_type pos) const {
         return vec_[pos];
@@ -48,15 +70,36 @@ public:
     typename vector_type::const_iterator heap_begin() const {return vec_.begin() + 1;}
     typename vector_type::const_iterator heap_end() const {return vec_.end();}
 
+    size_type last_pos() const {return vec_.size() -1; }
+
+    const value_type& value(size_type pos=ROOT_POS) const {return vec_[pos];}
+
+    children_type children(size_type pos) const {
+        if( auto left = 2*pos, right = left +1; left < last_pos()) {
+            return  {left, right}; // for most heaps with some elements this is the prevailing branch
+        } else if(left < size()) {
+            return  {left, 0}; // rare
+        } else {
+            return {0, 0}; // leaf
+        }
+    }
+    constexpr size_type root_pos() const { return ROOT_POS;}
 private:
     [[nodiscard]] size_type data_x_lt_y_(size_type l, size_type r) const {
         // returns l if l < r. 0 otherwise
         return Compare()(vec_[l].first, vec_[r].first) ? l : 0;
     }
+    bool is_x_lt_y_(size_type x, size_type y) const {
+        return Compare()(vec_[x].first, vec_[y].first);
+    }
     // if node is less than its parent returns the parents position
     // otherwise returns 0
     [[nodiscard]] size_type is_lt_parent(size_type pos) const {
-        return pos > 1 ? data_x_lt_y_(pos, pos/2) : 0;
+        auto parent_pos = pos/2;
+        if (pos > ROOT_POS) {
+            return is_x_lt_y_(pos, parent_pos) ? parent_pos :0;
+        } else
+            return 0;
     }
     // returns position of the smallest child which is smaller than `pos` node
     // if neither child is smaller than the pos or if there are no children return 0
@@ -73,15 +116,15 @@ private:
     // running up towards the root
     // grabbing the last element by default
     void filter_up_(size_type p = 0) {
-        for(size_type pos = (p ? p : vec_.size()-1); p > 1; ) {
+        for(size_type pos = (p ? p : last_pos()); pos > ROOT_POS; ) {
             if( auto parent_pos = is_lt_parent(pos)) {
                 swap_positions_(pos, parent_pos);
             } else
                 break;
         }
     }
-    // filter down tries to swap node closer to the root
-    void filter_down_(size_type p =1) {
+    // filter down tries to swap node away from the root
+    void filter_down_(size_type p = ROOT_POS) {
         for(auto pos = p; pos < vec_.size();) {
             if(auto child_pos = is_gt_child(pos); child_pos) {
                 swap_positions_(pos, child_pos);
@@ -98,5 +141,42 @@ private:
     std::vector<value_type> vec_;
 };
 
+template <typename DmaTree, typename Container=std::stack<typename DmaTree::size_type>>
+struct dma_tree_traverser {
+    using size_type = typename DmaTree::size_type;
+    using path_type = Container;
+
+    const DmaTree& tree_;
+
+    explicit dma_tree_traverser(const DmaTree& tree): tree_(tree) {}
+
+    template <typename CB=std::unary_function<size_type, bool>>
+    typename  DmaTree::size_type operator()(CB& cb) const {
+        path_type path;
+        path.push(tree_.root_pos());
+        size_type visited_count = 0;
+        while(!path.empty()) {
+            auto pos = path.pop();
+            visited_count ++;
+            if(!cb(pos))
+                break;
+
+            if( auto [left, right] = tree_.children(pos); left ) {
+                path.push(left);
+                if(right) {
+                    path.push(right);
+                }
+            }
+        }
+        return visited_count;
+    }
+
+};
+
+template <typename DmaTree>
+using dma_tree_dfs = dma_tree_traverser<DmaTree, std::stack<typename DmaTree::size_type>>;
+
+template <typename DmaTree>
+using dma_tree_bfs = dma_tree_traverser<DmaTree, std::queue<typename DmaTree::size_type>>;
 
 #endif //CPP_DMA_HEAP_H
--- tree/test/test_dma_heap.cpp
+++ tree/test/test_dma_heap.cpp
@@ -2,9 +2,41 @@
 // Created by andrey yanpolsky on 4/24/20.
 //
 #include "../dma_heap.h"
+#include <iostream>
+
+
+namespace {
+    template <typename Heap>
+    std::ostream& bfs_print(std::ostream& fp, const Heap& heap, int indent_width=4)  {
+        auto depth = heap.size();
+        for(; depth > 0; depth /=2);
+
+        int pos = 1;
+        std::vector<typename Heap::data_type> stack;
+
+        for(auto i = heap.begin(); i != heap.end(); i++, pos++ ) {
+            fp << std::string (indent_width*(pos/2), ' ') << i->first << std::endl;
+        }
+        return fp;
+    }
+}
 
 int main(int argc, const char* argv[]) {
-    dma_heap<int> heap;
-    heap.push_heap(10);
+    {
+        dma_heap<int> h1;
+        for(auto a: {1, 9, 7, 8}) {
+            h1.push_heap(a);
+        }
+        bfs_print(std::cout, h1);
+        dma_heap<int> h2;
+        for(auto a: {9, 10, 8, 6, 5, 7, 1}) {
+            h2.push_heap(a);
+        }
+        bfs_print(std::cout, h2);
+
+    }
+    {
+
+    }
 
 }
